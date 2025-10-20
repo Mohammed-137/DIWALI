958d6f373b2de1adfda40eff0a638d9bca96c885
Mohammed Yasar <yasarmohammed546@gmail.com>
2025-10-20 06:54:49 +0530

    Modify character spacing in script.js
    Adjusted character spacing for better text appearance.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
   const opts = {
     strings: ["HAPPY", "DIWALI MY DEAR FRIENDS AND MENTORS"],   // change text here
     charSize: 44,
-    charSpacing: 36, 
+    charSpacing: 32, 
     lineHeight: 72,
     gravity: 0.12,
     upFlow: -0.06,
